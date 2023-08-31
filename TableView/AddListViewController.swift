92ab68ec0e604d5ebf43e5645dff5e22899b6fb9
Nao0811 <naoto.7010.811@icloud.com>
2023-08-31 23:01:20 +0900

    再提出！

--- TableView/AddListViewController.swift
+++ TableView/AddListViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class AddListViewController: UIViewController {
 
+    @IBOutlet weak var addTextField: UITextField!
+    var taskArray: [String] = [] //入力したリストを格納する配列
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let userDefaults = UserDefaults.standard
+                if userDefaults.object(forKey: "add") != nil{
+                    taskArray = userDefaults.object(forKey: "add") as! [String]
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
+    @IBAction func addTaskaddTask(_ sender: Any) {
+        let userDefaults = UserDefaults.standard //そのままだと長いので変数にいれる
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    taskArray.append(addTextField.text!) //TextFieldで記入されたテキストを入れる
+
+    userDefaults.set(taskArray, forKey: "add") //キー"add"で配列をUserDefaultsに保存
+
+    self.navigationController?.popViewController(animated: true) //1つ前の画面に戻る
     }
-    */
-
+    
 }
--- TableView/TableViewController.swift
+++ TableView/TableViewController.swift
@@ -8,31 +8,37 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    var taskArray: [String] = []
     var names: [String] = [
-            "阿部",
-            "石田",
-            "上田",
-            "遠藤",
-            "及川",
-            "梶原",
-            "菊池",
-            "工藤",
-        ]
+        "阿部",
+        "石田",
+        "上田",
+        "遠藤",
+        "及川",
+        "梶原",
+        "菊池",
+        "工藤",
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         let userDefaults = UserDefaults.standard
-        if userDefaults.object(forKey: "add") != nil{
-            names = userDefaults.object(forKey: "add") as! [String]
+        //"add"というキーで保存された値がなにかある -> 値をtaskArrayへ
+        if userDefaults.object(forKey: "add") != nil {
+            taskArray = userDefaults.object(forKey: "add") as! [String]
         }
+        //tableViewを更新
         tableView.reloadData()
+        
     }
-
-    // MARK: - Table view data source
+    
+    
+    
     //セクションの数を返すメゾット
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
@@ -40,9 +46,7 @@ class TableViewController: UITableViewController {
     }
     //セクションの中の行(セル)の数
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        //配列の変数名.countで配列の要素数を取得できる
-        return names.count
+        return taskArray.count
     }
     
     //セルの表示内容を定設定する
@@ -51,55 +55,55 @@ class TableViewController: UITableViewController {
         //どのセルを使用するかを設定している
         let cell = tableView.dequeueReusableCell(withIdentifier: "NameCell", for: indexPath)
         //セルのラベルと配列の中身を表示
-        cell.textLabel?.text = names[indexPath.row]
-
+        cell.textLabel?.text = taskArray[indexPath.row]
+        
         return cell
     }
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
         let userDefaults = UserDefaults.standard
@@ -117,5 +121,5 @@ class TableViewController: UITableViewController {
         userDefaults.set(names, forKey: "add")
         
     }
-
+    
 }
